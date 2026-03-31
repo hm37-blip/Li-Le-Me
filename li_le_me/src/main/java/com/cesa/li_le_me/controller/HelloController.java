1a6b0d88c41a2f6fcf2c18087fed2c24d1dbe26c
CL-YQLM <ciciliu307@gmail.com>
2026-03-30 22:11:30 -0500

    springboot基础框架搭建完了

--- li_le_me/src/main/java/com/cesa/li_le_me/controller/HelloController.java
+++ li_le_me/src/main/java/com/cesa/li_le_me/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.cesa.li_le_me;
+package com.cesa.li_le_me.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
