b0ce36c68b13f0481031f3efb3a1983a3dc33ef4
CL-YQLM <ciciliu307@gmail.com>
2026-04-08 09:14:58 -0500

    ncluded a database file (lileme.mv.db)

---
+++ li_le_me/src/main/java/com/cesa/li_le_me/LiLeMeApplication.java
@@ -0,0 +1,13 @@
+package com.cesa.li_le_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiLeMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiLeMeApplication.class, args);
+	}
+
+}
